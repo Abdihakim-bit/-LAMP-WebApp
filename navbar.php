5e8beb3747d3175cb6015dae0d77b5dee00d3a2f
Abdihakim Ali <79899570+Abdihakim-bit@users.noreply.github.com>
2024-06-03 05:39:41 -0400

    Create navbar.php
    This commit introduces a dynamic navigation bar (navbar) for the website. The navbar adapts based on the user's login status, displaying different links accordingly.

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<?php
+// Start the session
+session_start();
+
+// Check if user is logged in
+if (isset($_SESSION['username'])) {
+    echo '<a href="index.php">Home</a>';
+    echo '<a href="#">Discussion Board</a>';
+    echo '<a href="#">Documents</a>';
+    echo '<a href="profile.php">Profile</a>';
+    echo '<a href="logout.php">Logout</a>';
+} else {
+    echo '<a href="index.php">Home</a>';
+    echo '<a href="sign-up.php">Sign Up</a>';
+    echo '<a href="sign-in.php">Sign In</a>';
+}
+?>
